daa1e108819cca5370f64cc8848f6a15de4a6b28
Raphelle <raphelle.lansaderas@aclcbutuan.edu.ph>
2024-07-11 19:11:03 -0700

    Add files via upload

---
+++ act5.py
@@ -0,0 +1,17 @@
+word_list = []
+
+def process_words(input_text):
+    current_word = ''
+    for char in input_text:
+        if char == ',':
+            if current_word:
+                word_list.append(current_word)
+                current_word = ''
+        else:
+            current_word += char
+    if current_word:
+        word_list.append(current_word)
+
+input_text = input("your sentence: ")
+process_words(input_text)
+print(word_list)
